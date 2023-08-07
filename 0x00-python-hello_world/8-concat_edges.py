404f6591f18f023c9ffa7b7a845d1ae0dc0cceba
Ikorli <ikorlit@gmail.com>
2023-08-07 16:42:15 -0700

    Creating new sentences that prints object-oriented programming with python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+text = "Python is an interpreted, interactive, object-oriented programming\
+ language that is used in many fields"
+new_text = text[39:66] + text[106:112] + text[:6]
+print(new_text)
