44838d68b9ac346d70d96a9d8c3053dd37f76cb3
Ikorli <ikorlit@gmail.com>
2023-08-07 16:45:04 -0700

    updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 text = "Python is an interpreted, interactive, object-oriented programming\
  language that is used in many fields"
-new_text = text[39:66] + text[106:112] + text[:6]
+new_text = "{}{}{}".format(text[39:66], text[106:112], text[:6])
 print(new_text)
