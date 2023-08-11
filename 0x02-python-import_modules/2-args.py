89e1101057911af72b85a85a8cfa37361be07ee1
Ikorli <ikorlit@gmail.com>
2023-08-11 15:21:50 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,13 @@
 if __name__ == "__main__":
 
     import sys
-    
-    total = len(sys.argv) - 1
-    if total == 0:
+
+    counter = len(sys.argv) - 1
+    if counter == 0:
         print("0 arguments.")
-    elif total == 1:
+    elif counter == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(total))
-    
-    for i in range(total):
-        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(counter))
+    for i in range(counter):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
