a50f6f88939cf289f7481cfc166294c60a96bd1a
Ikorli <ikorlit@gmail.com>
2023-08-11 14:52:51 -0700

    updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    return a + b if a < b else raise Exception('a must be less than b')
+    if a < b:
+        return a + b
+    else:
+        raise ValueError('a must be less than b')
