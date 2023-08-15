6ddfebfaa110dd019b856699965f6fcaebb3d800
Ikorli <ikorlit@gmail.com>
2023-08-15 14:32:29 +0000

    all fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,71 @@
 #include "lists.h"
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the beginning of the linked list.
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ * reverse_listint - Reverses a linked list.
+ * @head: Pointer to the first node in the list.
+ * Return: Pointer to the first node in the new list.
+ */
+void reverse_listint(listint_t **head)
+{
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
+
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+}
+
+/**
+ * is_palindrome - Checks if a linked list is a palindrome.
+ * @head: Double pointer to the linked list.
+ *
+ * Return: 1 if it is a palindrome, 0 if not.
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *temp;
+    listint_t *slow = *head, *fast = *head, *temp = *head, *reversed = NULL;
 
     if (*head == NULL || (*head)->next == NULL)
         return (1);
 
-    while (fast != NULL && fast->next != NULL)
+    while (1)
     {
         fast = fast->next->next;
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
+        if (!fast)
+        {
+            reversed = slow->next;
+            break;
+        }
+        if (!fast->next)
+        {
+            reversed = slow->next->next;
+            break;
+        }
+        slow = slow->next;
     }
 
-    if (fast != NULL)
-        slow = slow->next;
+    reverse_listint(&reversed);
 
-    while (prev != NULL && slow != NULL)
+    while (reversed && temp)
     {
-        if (prev->n != slow->n)
+        if (temp->n == reversed->n)
+        {
+            reversed = reversed->next;
+            temp = temp->next;
+        }
+        else
             return (0);
-        prev = prev->next;
-        slow = slow->next;
     }
 
-    return (1);
+    if (!reversed)
+        return (1);
+
+    return (0);
 }
