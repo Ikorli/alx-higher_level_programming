7ac4ea498b8c42d3884b0553f31fe54ff1332a6e
Ikorli <ikorlit@gmail.com>
2023-08-15 13:50:45 +0000

    fixed pycode style error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
 
+def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+
     max_value = my_list[0]
     for i in range(1, len(my_list)):
         # Update the max value if the current element is larger
         if my_list[i] > max_value:
             max_value = my_list[i]
-    
+
     return max_value
