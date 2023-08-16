1863e4a3c04ea490c8cb85adaa1a5dbf6c22f1be
Ikorli <ikorlit@gmail.com>
2023-08-16 13:20:43 -0700

    updated

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return sum([num for num in my_list if my_list.count(num) == 1])
+    unique_sum = 0
+    seen = set()
+    for num in my_list:
+        if num not in seen:
+            unique_sum += num
+            seen.add(num)
+    return unique_sum
