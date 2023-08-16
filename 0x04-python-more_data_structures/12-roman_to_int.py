4eeb9f34f75c7840edf894a6b928e4a55a43227a
Ikorli <ikorlit@gmail.com>
2023-08-16 14:51:31 -0700

    returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     total = 0
-    roman_values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    prev_value = 0
-    
-    for roman in reversed(roman_string):
-        int_value = roman_values.get(roman, 0)
-        
-        if int_value < prev_value:
-            total -= int_value
-        else:
-            total += int_value
-        
-        prev_value = int_value
-    
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for i in range(len(roman_string) - 1, -1, -1):
+        intVal = digits[roman_string[i]]
+        total += intVal if total < intVal * 5 else -intVal
     return total
