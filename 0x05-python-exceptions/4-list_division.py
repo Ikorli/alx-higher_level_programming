989f1a3593551b26cfafdcd1fd584111711a79ff
Ikorli <ikorlit@gmail.com>
2023-08-28 11:33:53 -0700

    updated

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 list_division = __import__('4-list_division').list_division
 
+def test_list_division(my_l_1, my_l_2):
+    max_length = max(len(my_l_1), len(my_l_2))
+    result = list_division(my_l_1, my_l_2, max_length)
+    return result
+
+# Test case 1
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
-
-try:
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-except (ZeroDivisionError, TypeError, IndexError):
-    result = [0] * max(len(my_l_1), len(my_l_2))
-
+result = test_list_division(my_l_1, my_l_2)
 print(result)
 
 print("--")
 
+# Test case 2
 my_l_1 = [10, 8, 4, 4]
 my_l_2 = [2, 0, "H", 2, 7]
-
-try:
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-except (ZeroDivisionError, TypeError, IndexError):
-    result = [0] * max(len(my_l_1), len(my_l_2))
-
+result = test_list_division(my_l_1, my_l_2)
 print(result)
